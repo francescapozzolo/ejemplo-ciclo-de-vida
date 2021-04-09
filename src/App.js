3c0d8af91c24b832506dcc47add4e7bd40199ef6
Fernando Biaus <biaus.fh@gmail.com>
2021-04-09 12:21:32 -0300

    Fin ejemplo con clases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    productos: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    this.fetchearApi()
+  }
+
+  fetchearApi = () => {
+    fetch('https://apipetshop.herokuapp.com/api/articulos')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        productos: data.response,
+        loading: false
+      })
+
+    })
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.productos.length > 0) {
+      if (nextState.productos[0].nombre !== this.state.productos[0].nombre) {
+        return false
+      }
+    }    
+    return true
+  }
+
+  render() {
+    console.log("RENDER")
+    if (this.state.loading) {
+      return <h1>CARGANDO....</h1>
+    }
+
+    return (
+      <div style={{width: '80vw', height: '80vh', margin: '3vh auto', backgroundColor: 'yellow'}}>
+        <button onClick={this.fetchearApi}>Pedir productos de nuevo</button>
+       {this.state.productos.length === 0
+       ? <h1>No hay productos</h1>
+       : this.state.productos.map(producto => {
+        return (
+          <h1>{producto.nombre} ({producto.tipo})</h1>
+        )
+       })}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
